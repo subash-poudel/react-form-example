8bc6c67292015430f8f6ee8a3a185728b085d3f5
npsedhain <npsedhain@gmail.com>
2020-12-22 14:40:34 +0545

    widget: add minimal css

--- public/widget.js
+++ public/widget.js
@@ -1,38 +1,47 @@
-const WIDGET_PREFIX = "vyaguta-search-";
-const INPUT_ELEMENT_ID = WIDGET_PREFIX + "input";
-const BUTTON_ELEMENT_ID = WIDGET_PREFIX + "button";
-const RESULTS_DIV_ID = WIDGET_PREFIX + "results";
-const LIST_ITEM_CLASS = WIDGET_PREFIX + "search-result";
+const WIDGET_PREFIX = 'vyaguta-search-';
+const INPUT_DIV_ID = WIDGET_PREFIX + 'input-container';
+const INPUT_ELEMENT_ID = WIDGET_PREFIX + 'input';
+const BUTTON_ELEMENT_ID = WIDGET_PREFIX + 'button';
+const RESULTS_DIV_ID = WIDGET_PREFIX + 'results';
+const LIST_ITEM_CLASS = WIDGET_PREFIX + 'search-result';
 
 function getButton() {
-  const button = document.createElement("BUTTON");
-  button.innerHTML = "Search";
+  const button = document.createElement('BUTTON');
+  button.innerHTML = 'Search';
   button.id = BUTTON_ELEMENT_ID;
-  button.addEventListener("click", handleSearch);
+  button.addEventListener('click', handleSearch);
   return button;
 }
 
+function getInputDiv() {
+  const inputDiv = document.createElement('DIV');
+  inputDiv.id = INPUT_DIV_ID;
+  return inputDiv;
+}
+
 function getInput() {
-  const inputElement = document.createElement("INPUT");
-  inputElement.placeholder = "Vyaguta Search";
+  const inputElement = document.createElement('INPUT');
+  inputElement.placeholder = 'Vyaguta Search';
   inputElement.id = INPUT_ELEMENT_ID;
   return inputElement;
 }
 
 function getResultsDiv() {
-  const resultDiv = document.createElement("DIV");
+  const resultDiv = document.createElement('DIV');
   resultDiv.id = RESULTS_DIV_ID;
   return resultDiv;
 }
 
 function initializeWidget() {
-  const parent = document.getElementById("vyaguta-widget");
+  const parent = document.getElementById('vyaguta-widget');
   clearChildren(parent);
+  const inputDiv = getInputDiv();
   const inputElement = getInput();
   const button = getButton();
   const resultsDiv = getResultsDiv();
-  parent.appendChild(inputElement);
-  parent.appendChild(button);
+  parent.appendChild(inputDiv);
+  inputDiv.appendChild(inputElement);
+  inputDiv.appendChild(button);
   parent.appendChild(resultsDiv);
 }
 
@@ -44,15 +53,14 @@ function fetchSearchResults(term) {
 }
 
 function getSearchResultDiv(data) {
-  const aTag = document.createElement("A");
+  const aTag = document.createElement('A');
   aTag.className = LIST_ITEM_CLASS;
   aTag.innerHTML = data.name;
-  aTag.target = "_blank";
-  aTag.rel = "noopener noreferrer";
-  aTag.href = `https://en.wikipedia.org/wiki/${data.name}`
+  aTag.target = '_blank';
+  aTag.rel = 'noopener noreferrer';
+  aTag.href = `https://en.wikipedia.org/wiki/${data.name}`;
   return aTag;
 }
-
 
 //https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
 function clearChildren(parent) {
@@ -64,6 +72,13 @@ function clearChildren(parent) {
 function showSearchResults(countries) {
   const parent = document.getElementById(RESULTS_DIV_ID);
   clearChildren(parent);
+
+  if (!countries.length) {
+    parent.style.height = '0px';
+    return;
+  }
+
+  parent.style.height = '200px';
   countries.forEach((data) => {
     const result = getSearchResultDiv(data);
     parent.appendChild(result);
@@ -79,8 +94,8 @@ function handleSearch() {
 }
 
 function destroy() {
-  const parent = document.getElementById("vyaguta-widget");
-  clearChildren(parent)
+  const parent = document.getElementById('vyaguta-widget');
+  clearChildren(parent);
 }
 
 window.vyagutaSearch = {
@@ -88,4 +103,4 @@ window.vyagutaSearch = {
   destroy: destroy
 };
 
-console.log(window.vyagutaSearch, 'in script')
+console.log(window.vyagutaSearch, 'in script');
